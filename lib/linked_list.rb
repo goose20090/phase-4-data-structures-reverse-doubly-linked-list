6c7d6e4ed40d891b7f4d1b5d75f45e494562ab85
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-05-09 17:52:31 +0100

    commit tree 7bfe325f9c04387c9dc3295b2227fc13f4444c53

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class LinkedList
   attr_accessor :head, :tail
@@ -9,6 +10,42 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    return if @head.nil? || @head.next_node.nil? # If the list is empty or has only one node, do nothing.
+
+    current_node = @head
+
+    while current_node
+      # Swap prev_node and next_node
+      temp = current_node.prev_node
+      current_node.prev_node = current_node.next_node
+      current_node.next_node = temp
+
+      # Move to the next node (which is the previous node after swapping)
+      if current_node.prev_node
+        current_node = current_node.prev_node
+      else
+        break
+      end
+    end
+
+    # Swap head and tail
+    temp = @head
+    @head = @tail
+    @tail = temp
   end
 end
+
+# list = LinkedList.new
+
+# node_1 = Node.new(1)
+# node_2 = Node.new(2)
+# node_3 = Node.new(3)
+  
+# list.head = node_1        # h1
+# node_1.next_node = node_2 # h1 --> 2
+# node_2.prev_node = node_1 # h1 <--> 2
+# node_2.next_node = node_3 # h1 <--> 2 --> 3
+# node_3.prev_node = node_2 # h1 <--> 2 <--> 3
+# list.tail = node_3        # h1 <--> 2 <--> 3t
+
+# list.reverse!
